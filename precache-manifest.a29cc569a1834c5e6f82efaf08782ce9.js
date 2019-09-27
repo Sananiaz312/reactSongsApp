b2124300742073b949f71562f3feb56b5a2557f3
Sananiaz312 <Sananiaz312@gmail.com>
2019-09-26 21:59:25 -0700

    Updates

---
+++ precache-manifest.a29cc569a1834c5e6f82efaf08782ce9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "854c60c2f1e97991359b1f78f9fdbdcf",
+    "url": "/Sananiaz312/reactSongsApp/index.html"
+  },
+  {
+    "revision": "68a029fb9bef7c946a95",
+    "url": "/Sananiaz312/reactSongsApp/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "287ace0e0ca95f137c24",
+    "url": "/Sananiaz312/reactSongsApp/static/js/2.49294686.chunk.js"
+  },
+  {
+    "revision": "68a029fb9bef7c946a95",
+    "url": "/Sananiaz312/reactSongsApp/static/js/main.a66fbbda.chunk.js"
+  },
+  {
+    "revision": "60d17c6ca3ea1ff0f5b5",
+    "url": "/Sananiaz312/reactSongsApp/static/js/runtime-main.6874cdc5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Sananiaz312/reactSongsApp/static/media/logo.25bf045c.svg"
+  }
+]);
